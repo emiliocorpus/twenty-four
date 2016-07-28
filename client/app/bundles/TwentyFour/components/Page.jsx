c78e3ab6aba412a538bc6f9cce3f9e5d46c96917
emiliocorpus <emiliocorpus@gmail.com>
2016-07-27 19:00:46 -0700

    changes hello world directory to twent-four, configures component registration

---
+++ client/app/bundles/TwentyFour/components/Page.jsx
@@ -0,0 +1,20 @@
+import React, { PropTypes } from 'react';
+import _ from 'lodash';
+
+// Simple example of a React "dumb" component
+export default class Page extends React.Component {
+  
+  constructor(props, context) {
+    super(props, context);
+  }
+
+ 
+
+  render() {
+    return (
+      <div className="container">
+        haksdhfjklasjdhfkashdfkajhsdfklajsdhf
+      </div>
+    );
+  }
+}
--- client/app/bundles/TwentyFour/startup/clientRegistration.jsx
+++ client/app/bundles/TwentyFour/startup/clientRegistration.jsx
@@ -1,5 +1,7 @@
 import ReactOnRails from 'react-on-rails';
 import HelloWorldApp from './HelloWorldAppClient';
+import Page from '../components/Page';
 
 // This is how react_on_rails can see the HelloWorldApp in the browser.
 ReactOnRails.register({ HelloWorldApp });
+ReactOnRails.register({Page})
